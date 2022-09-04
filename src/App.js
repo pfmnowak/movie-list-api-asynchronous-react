37db3575b7f3fdddde42559eb259da26c34d92f4
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-04 22:44:53 +0200

    Fetch movies from the API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from 'react';
-
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import MoviesList from "./components/MoviesList";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        releaseDate: movie.release_date,
+        openingText: movie.opening_crawl,
+      };
+    });
+    setMovies(transformedMovies);
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
